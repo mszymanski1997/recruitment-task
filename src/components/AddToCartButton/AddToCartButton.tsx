1d7ca15222793173875e39f8b6596838bd10b57b
Mateusz Szymański <Mizerykordia12@interia.pl>
2026-05-01 17:44:49 +0200

    feat: add loading and success states with visual feedback to AddToCartButton

--- src/components/AddToCartButton/AddToCartButton.tsx
+++ src/components/AddToCartButton/AddToCartButton.tsx
@@ -7,19 +7,31 @@ import { useState } from 'react';
 const AddToCartButton = () => {
 	const { addToCart } = useCart();
 	const [isAdding, setIsAdding] = useState(false);
+	const [isAdded, setIsAdded] = useState(false);
 
 	const addItem = () => {
 		setIsAdding(true);
 
 		setTimeout(() => {
-            setIsAdding(false);
+			setIsAdding(false);
 			addToCart();
+			setIsAdded(true);
 		}, 1000);
+
+		setTimeout(() => {
+			setIsAdded(false);
+		}, 1500);
 	};
 
 	return (
-		<button className={styles.button} onClick={addItem}>
-			{isAdding ? 'Adding...' : 'Add to Cart'}
+		<button
+			className={`${styles.button} ${isAdded && styles.isAdded}`}
+			onClick={addItem}
+			disabled={isAdding}
+		>
+			{!isAdding && !isAdded && 'Add to Cart'}
+			{isAdding && 'Adding...'}
+			{isAdded && 'Added!'}
 		</button>
 	);
 };
